9c9b936ab2241abbca70203110ad08ec92c55efd
Terrence Nixa <tnixa@us.ibm.com>
2019-05-08 14:20:08 -0500

    changes for spring starter v7.0.0

--- src/main/java/org/terrence/testapp/rest/TestRestController.java
+++ src/main/java/org/terrence/testapp/rest/TestRestController.java
@@ -7,11 +7,12 @@ import java.io.StringWriter;
 
 import com.google.gson.stream.JsonReader;
 
-import com.ibm.watson.developer_cloud.personality_insights.v3.PersonalityInsights;
-import com.ibm.watson.developer_cloud.personality_insights.v3.model.Content;
-import com.ibm.watson.developer_cloud.personality_insights.v3.model.Profile;
-import com.ibm.watson.developer_cloud.personality_insights.v3.model.ProfileOptions;
-import com.ibm.watson.developer_cloud.util.GsonSingleton;
+import com.ibm.watson.personality_insights.v3.PersonalityInsights;
+import com.ibm.watson.personality_insights.v3.model.Content;
+import com.ibm.watson.personality_insights.v3.model.Profile;
+import com.ibm.watson.personality_insights.v3.model.ProfileOptions;
+
+import com.ibm.cloud.sdk.core.util.GsonSingleton;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,7 +41,7 @@ public class TestRestController {
       ProfileOptions profileOptions = new ProfileOptions.Builder().content(content).consumptionPreferences(true)
           .rawScores(true).build();
       pw.println("Creating Personality Insights from profile.json file");
-      Profile profile = personalityInsights.profile(profileOptions).execute();
+      Profile profile = personalityInsights.profile(profileOptions).execute().getResult();
       System.out.println("Profile Results: " + profile);
 
       // check to see if query exists in the results
